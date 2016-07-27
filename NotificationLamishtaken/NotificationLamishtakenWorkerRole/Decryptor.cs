ae1e4825bbe704a1e9719a397c148fd3ecae7262
kfiro <kfiro@KFIRO-990SSD>
2016-07-27 14:30:29 +0300

    add decryptor

---
+++ NotificationLamishtaken/NotificationLamishtakenWorkerRole/Decryptor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.Pkcs;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotificationLamishtakenWorkerRole
+{
+    public static class Decryptor
+    {
+        static string Decrypt(string encryptedItem)
+        {
+            if (string.IsNullOrWhiteSpace(encryptedItem))
+            {
+                return encryptedItem;
+            }
+
+            var envelopedCms = new EnvelopedCms();
+            envelopedCms.Decode(Convert.FromBase64String(encryptedItem));
+            envelopedCms.Decrypt();
+            return Encoding.UTF8.GetString(envelopedCms.ContentInfo.Content);
+        }
+    }
+}
